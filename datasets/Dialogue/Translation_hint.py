1ddf1466658a110eeb82c7c5838b5c3970743ab6
rojadyn <ri.rojadyn@gmail.com>
2019-02-11 15:36:08 -0800

    final fixes to syntax issues and labels

--- datasets/Dialogue/Translation_hint.py
+++ datasets/Dialogue/Translation_hint.py
@@ -1,3 +1,3 @@
 {'name': 'Translation_hint',
  'columns': [('ID', None),
-             ('Dialogue in English (other languages available upon request)', None)]}
+             ('English', None, {'label': 'Dialogue in English (other languages available upon request)'})]}
--- datasets/Furniture/Item_StorageBox.py
+++ datasets/Furniture/Item_StorageBox.py
@@ -1,4 +1,4 @@
 {'name': 'Item_StorageBox',
  'columns': [('id', Lookup.PROP, {'label': 'Name of storage box'}),
              ('Count', None, {'label': 'Storage slots available'}),
-             ('npcinteract', Transform.BOOL, {'label': 'NPCs can use it?'})]}
+             ('npcinteract', None, {'label': 'NPCs can use it?'})]}
