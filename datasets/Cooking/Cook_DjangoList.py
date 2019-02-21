723b68ef62d5f8f52a05f274ba878df30e65692d
rojadyn <ri.rojadyn@gmail.com>
2019-02-21 14:17:59 -0800

    added Cook_DjangoList (based on favor with Django)

---
+++ datasets/Cooking/Cook_DjangoList.py
@@ -0,0 +1,4 @@
+{'name': 'Cook_DjangoList',
+ 'columns': [('Favor', None, {'label': 'Favor (with Django?'}),
+             ('UnlockList', Lookup.PROP, {'split': ',',
+                                          'label': 'Unlocked recipes'})]}
