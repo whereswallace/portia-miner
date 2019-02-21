d81dd9858c388ae3b740d68669205a08ce8b3286
rojadyn <ri.rojadyn@gmail.com>
2019-02-21 11:37:52 -0800

    added AckDataChips to Items

---
+++ datasets/Items/AckDataChips.py
@@ -0,0 +1,5 @@
+{'name': 'AckDataChips',
+ 'columns': [('ItemID', Lookup.PROP, {'label': 'Item ID'}),
+             ('Name', Lookup.TRANSLATION, {'label': 'Item name'}),
+             ('Param', Lookup.PROP, {'label': 'Associated recipe'}),
+             ('Type', None)]}
