790d181609d55e82511bc8165cfa7d08db4d806b
rojadyn <ri.rojadyn@gmail.com>
2019-02-21 15:48:01 -0800

    added Creation_Book (notes from handbook)

---
+++ datasets/Crafting/Creation_Book.py
@@ -0,0 +1,5 @@
+{'name': 'Creation_Book',
+ 'columns': [('ItemId', Lookup.PROP, {'label': 'Name of handbook ingredient'}),
+             ('Des', Lookup.TRANSLATION, {'label': 'Handbook notes/tips'}),
+             ('Tips', Lookup.TRANSLATION, {'split': '_',
+                                           'label': 'More notes/tips'})]}
