24f7a84a6db62c7d14234b2ea0b4980abfbb4a88
rojadyn <ri.rojadyn@gmail.com>
2019-02-21 14:08:47 -0800

    added weight and quality fields to Cook_Book (don't understand them yet)

--- datasets/Cooking/Cook_Book.py
+++ datasets/Cooking/Cook_Book.py
@@ -2,4 +2,6 @@
  'columns': [('Food', Lookup.PROP, {'label': 'Recipe'}),
              ('Material', [Lookup.PROP, Lookup.STORE_PRODUCT, Lookup.COOK_TAG_NAME], {'quantity_post': '_',
                                                                                       'split': ';',
-                                                                                      'label': 'Ingredients'})]}
+                                                                                      'label': 'Ingredients'}),
+             ('Weight', None, {'label': '**Weight**'}),
+             ('Quality', None, {'label': '**Quality**'})]}
