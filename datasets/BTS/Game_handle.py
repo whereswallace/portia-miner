3e5ecf46026c6592aa3f153b0ac97e2977072bb9
rojadyn <ri.rojadyn@gmail.com>
2019-02-21 18:23:56 -0800

    added Game_handle (PS4 and XBOX controls guide)

---
+++ datasets/BTS/Game_handle.py
@@ -0,0 +1,4 @@
+{'name': 'Game_handle',
+ 'columns': [('function', None, {'label': 'Game action'}),
+             ('PS4', None, {'label': 'PS4 controls'}),
+             ('XBOX', None, {'label': 'Xbox controls'})]}
