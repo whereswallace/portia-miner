ebcba6f4f1f956c87160751474bc31fb08da8bba
rojadyn <ri.rojadyn@gmail.com>
2019-02-21 18:01:47 -0800

    added Fish_collect (different versions of each fish)

---
+++ datasets/Fish/Fish_collect.py
@@ -0,0 +1,4 @@
+{'name': 'Fish_collect',
+ 'columns': [('smallId', Lookup.PROP, {'label': 'Common fish'}),
+             ('largeId', Lookup.PROP, {'label': 'Large version'}),
+             ('KingId', Lookup.PROP, {'label': 'King version'})]}
