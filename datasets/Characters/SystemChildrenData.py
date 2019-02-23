f89e15747e849a63f16cf7446ef583b4067fbe61
rojadyn <ri.rojadyn@gmail.com>
2019-02-22 18:04:38 -0800

    added SystemChildrenData (calculating baby characteristics)

---
+++ datasets/Characters/SystemChildrenData.py
@@ -0,0 +1,9 @@
+{'name': 'SystemChildrenData',
+         'columns': [('npc_Id', Lookup.NPC_ID_TO_NAME, {'label': 'Name of character'}),
+                     ('ActorData', None, {'label': '**ActorData**'}),
+                     ('HairType', None, {'label': '**HairType**'}),
+                     ('FaceType', None, {'label': '**FaceType**'}),
+                     ('HairColor', None, {'label': '**HairColor**'}),
+                     ('SkinColor', None, {'label': '**SkinColor**'}),
+                     ('EyeColor', None, {'label': '**EyeColor**'}),
+                     ('LipColor', None, {'label': '**LipColor**'})]}
