e0c102ae8870445faf1cc101bf4367fd4bc55f39
rojadyn <ri.rojadyn@gmail.com>
2019-02-22 19:18:01 -0800

    added Unlimited_DungeonN, Unlimited_First Reward (Deepest Ruin)

---
+++ datasets/Dungeons/Unlimited_FirstReward.py
@@ -0,0 +1,4 @@
+{'name': 'Unlimited_FirstReward',
+ 'columns': [('RewardLevel', None, {'label': 'Level in Deepest Ruin'}),
+             ('First_Reward', Lookup.PROP, {'quantity_post': '_',
+                                            'label': 'Reward for first time reaching level'})]}
