4ba885d6f67423f3576f7d667d87160477501ce1
rojadyn <ri.rojadyn@gmail.com>
2019-02-22 20:15:07 -0800

    added Voxel_Herbs_Limit (abandoned ruins limited items)

---
+++ datasets/Gathering/Voxel_Herbs_Limit.py
@@ -0,0 +1,4 @@
+{'name': 'Voxel_Herbs_Limit',
+ 'columns': [('ID', Lookup.RANDOM_HERBS_ITEM_DROP_FIXED, {'label': 'Fixed drop from Abandoned Ruins'}),
+             ('ID', Lookup.RANDOM_HERBS_ITEM_DROP_RANDOM, {'label': 'Random drop from Abandoned Ruins'}),
+             ('MaxCount', None, {'label': 'Maximum amount'})]}
